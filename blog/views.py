0a0fe02b471cc871c946ca1c4964b997c252fc8f
arneladedovic <arneladedovic@hotmail.com>
2022-09-14 05:09:53 +0000

    Install allauth

--- blog/views.py
+++ blog/views.py
@@ -16,3 +16,31 @@ class PostList(generic.ListView):
     queryset = Post.objects.order_by('-created_on')
     template_name = 'post_list.html'
     paginate_by = 6
+
+
+class PostDetail(View):
+    """
+    Displays detailpost
+    """
+
+    def get(self, request, slug, *args, **kwargs):
+        """
+        Gets detailed post
+        """
+        queryset = Post.objects
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by("created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
